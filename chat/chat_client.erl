e0a231d61f3b3d8878c9720dddb6c5ec808c8d4c
Redbug <l314kimo@gmail.com>
2011-09-04 12:08:46 +0800

    removed .beam file and .swp file

--- chat/chat_client.erl
+++ chat/chat_client.erl
@@ -3,6 +3,12 @@
 
 send_message(RouterPid, Addressee, MessageBody) ->
     message_router:send_chat_message(RouterPid, Addressee, MessageBody).
+
+print_message(MessageBody) ->
+    io:format("Received: ~p~n", [MessageBody]).
+
+start_router() ->
+    message_router:start(fun chat_client:print_message/1).
 % c(chat_client.erl).
 % chat_client:send_message(P, P, "Hello from a client!").
 % P1 = spawn(message_router, route_messages, []).
--- chat/message_router.erl
+++ chat/message_router.erl
@@ -2,8 +2,8 @@
 
 -compile(export_all).
 
-start() ->
-    spawn(message_router, route_messages, []). 
+start(PrintFun) ->
+    spawn(message_router, route_messages, [PrintFun]). 
 
 stop(RouterPid) ->
     RouterPid ! shutdown.
@@ -11,19 +11,19 @@ stop(RouterPid) ->
 send_chat_message(RouterPid, Addressee, MessageBody) ->
     RouterPid ! {send_chat_msg, Addressee, MessageBody}.
 
-route_messages() ->
+route_messages(PrintFun) ->
     receive
 	{send_chat_msg, Addressee, MessageBody} ->
 	    Addressee ! {recv_chat_msg, MessageBody},
-	    route_messages();
+	    route_messages(PrintFun);
 	{recv_chat_msg, MessageBody} ->
-	    io:format("Receive: ~p~n", [MessageBody]),
-	    route_messages();
+	    PrintFun(MessageBody),
+	    route_messages(PrintFun);
 	shutdown ->
 	    io:format("Shutting down!~n");
 	Oops ->
 	    io:format("Warning! Receive: ~p~n", [Oops]),
-	    route_messages()
+	    route_messages(PrintFun)
     end.
 
 
